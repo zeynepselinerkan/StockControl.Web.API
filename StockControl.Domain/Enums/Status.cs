be48c764d4f14fed5b9df113d4fb13ff52d1cb58
Zeynep Selin Erkan <zeynepselinerkan@gmail.com>
2023-08-29 18:19:59 +0300

    Initial commit.

---
+++ StockControl.Domain/Enums/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockControl.Domain.Enums
+{
+    public enum Status // Sipariş durumu --> bekliyor, onaylanmış gibi...
+
+    {
+        Pending = 0,
+        Cancelled= 1,
+        Confirmed=3 
+
+    }
+}
